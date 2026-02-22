e0419e7f3a64641d66bd2038c94ec16007f72df6
lewis broadhurst <lewis1broadhurst@gmail.com>
2026-02-22 18:42:40 +0000

    add google tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import {Theme} from "@radix-ui/themes";
 
 import './globals.css'
 import "@radix-ui/themes/styles.css";
+import Script from "next/script";
 
 const inter = Inter({subsets: ['latin']})
 
@@ -14,10 +15,24 @@ export const metadata: Metadata = {
 }
 
 export default function RootLayout({children}: { children: React.ReactNode }) {
+    const googleTagId = process.env.NEXT_PUBLIC_GOOGLE_TAG_ID
 
     return (
         <html lang="en">
         <body className={inter.className}>
+        <Script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${googleTagId}`}
+            strategy="afterInteractive"
+        />
+        <Script id="google-gtag-init" strategy="afterInteractive">
+            {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${googleTagId}');
+          `}
+        </Script>
         <Theme appearance='dark' accentColor='gold' grayColor='sand' panelBackground='translucent'>
             {children}
         </Theme>
